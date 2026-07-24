dbcd82713f4dc39908f0d97d37d8c4e6b44e5d28
busraoren <busraoren0@hotmail.com>
2026-07-25 01:11:42 +0300

    kütüphane ekleme hata düzeltildi.

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 from django.contrib import admin
 from django.urls import path, include
+from burcs import views as burcs_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
