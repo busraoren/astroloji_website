6256165be9a8a8ff4e641c0eeb4309440cc4cfd3
busraoren <busraoren0@hotmail.com>
2026-07-24 09:24:51 +0300

    canlı ortam ayarları

--- config/settings.py
+++ config/settings.py
@@ -29,10 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%6%!zw5*bz_^enx6yq1s4vswztg=bh7cdk6ih1a)77jt=ww=gc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['astrobacilar.space', 'www.astrobacilar.space', '.railway.app']
 
 # Application definition
 
