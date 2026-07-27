711d5e79fa3e1dd8041e24c8686fdb94fa0d879f
busraoren <busraoren0@hotmail.com>
2026-07-27 22:19:57 +0300

    kullanıcı düzletildi

--- kullanicilar/views.py
+++ kullanicilar/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # DİKKAT: Profil modelinin adı farklıysa (örneğin UserProfile), aşağıdaki 'Profil' yazan yerleri değiştirmelisin.
-from .models import User
+from .models import KullaniciProfili
 
 
 @login_required
 def profil(request):
     # Kullanıcının profili varsa alır, yoksa veritabanında anında boş bir tane oluşturur.
     # Sayfanın "Profil yok" diyerek çökmesini kökten çözer.
-    kullanici_profili, created = User.objects.get_or_create(user=request.user)
+    kullanici_profili, created = KullaniciProfili.objects.get_or_create(user=request.user)
 
     context = {
         'profil': kullanici_profili
