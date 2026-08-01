6520a2556710dddd582ff77cdf607d7961fb6b02
busraoren <busraoren0@hotmail.com>
2026-08-01 13:18:26 +0300

    deploy ayarları güncellemesi

--- config/settings.py
+++ config/settings.py
@@ -94,12 +94,13 @@ import dj_database_url
 
 import dj_database_url
 
+import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
